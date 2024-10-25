429dedc4696dc708a5d38fb6f724e97e3366e212
Bilal Ahmad <bilalahmad9875@gmail.com>
2024-10-25 23:36:52 +0530

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [link,setLink]=useState("");
+const [Summary,setSummary]=useState("");
+const[loading,setLoading]=useState(false);
+
+const fetchSummary=async()=>{
+  setLoading(true);
+  const summary=await fetch(`https://article-extractor-and-summarizer.p.rapidapi.com/summarize?url=${link}&lang=en&engine=2`,{
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '24783ec466msh3a954ec6f5b83abp16a6c7jsnf4eb425c84de',
+      'x-rapidapi-host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+  });
+  const data=await summary.json();
+  console.log(data);
+  setSummary(data);
+  setLoading(false);
+  return data;
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+    <h2>Article Summarizer Application</h2>
+  <input type='url' placeholder="Enter article URL"  onChange={(e)=>{setLink(e.target.value)}}></input>
+  <button onClick={fetchSummary}>Click</button>
+  {loading? <p>Loading summary..... </p> : (
+  Summary.summary && <p className='summary'>{Summary.summary}</p> 
+)}
+  {/* short cicuiting of logical operator */}
     </div>
   );
 }
